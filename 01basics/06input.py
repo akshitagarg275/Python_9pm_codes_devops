c105745af856a3c5d05b994c6b52fb3b64901e5b
akshitagarg275 <akshitagarg275@gmail.com>
2023-06-21 21:52:43 +0530

    input and operators

---
+++ 01basics/06input.py
@@ -0,0 +1,13 @@
+# input -> it allows to take input from the user
+# By default it will always take input in the string
+
+# name = input("Please enter your name: ")
+# print("Hey! ",name)
+# print("Type of name: ",type(name))
+
+# age = int(input("Enter your age:"))
+# print("Your age is:",age)
+# print("Type of age is:", type(age))
+
+
+
---
+++ 01basics/07operators.py
@@ -0,0 +1,99 @@
+'''
+Operators -> It allows us to performn the operations
+
+there are two types of operators
+unary operator -> Only one operand is required
+
+binary operators -> two operands are required
+Arithmetic Opeators
+Assignment Operators
+Relational Operators
+Logical Operators
+Binary Operators
+
+Membership Operator
+Identity Operator
+'''
+
+# TODO: Unary Operators
+
+# a = -2
+# print(a)
+
+# TODO: Binary Operators
+# TODO: Assignment Operator '='
+# NOTE: It assigns RHS literal value to LHS variable
+
+# a = 5
+# print(a)
+
+# TODO: short hand notations
+
+x = 2
+# print("x before: ", x)
+# x = x + 3
+# x += 3
+
+#  x = x * 5
+x *= 5
+# print("x after: ", x)
+
+
+# TODO: Arithmetic Operators
+'''
++ , - , / , *
+
+** -> exponentation
+% -> modulus (remainder)
+// -> floor divison (providing integer quotient)
+/ -> decimal quotient
+'''
+
+num1 = 5
+num2 = 2
+
+# print("Sum is :", num1+num2)
+# print("Subtraction ans is :", num1 - num2)
+# print("Multipliplication ans is :", num1 * num2)
+# print("Divison ans is:", num1/num2)
+
+# print("floor divison ans is :", num1 // num2)
+# print("Remainder is: ", num1%num2)
+
+# print("5 raise to the power 2 is: ", 5**2)
+
+# TODO: Relational operator
+'''
+It returns boolean value (True/False)
+
+> : Greater than
+< : Lesser than
+
+NOTE: It will return True if either LHS value is greater/lesser than or equal to RHS value
+>= : Greater than equal to
+<= : Lesser than equal to
+
+
+== : Equality
+!= : Not equal to
+
+'''
+
+num1 = 5
+num2 = 2
+
+# print(num1 > num2)
+# print(3 > 7)
+# print(4 < 2)
+
+# print(5 >= 2)
+# print(5 >= 5)
+
+# NOTE: '=' is assignment operator  whereas '==' is relational operator
+
+
+# print( 5 == 5)
+# NOTE: It will prompt an error
+# print(5 = 5)
+
+print(5 != 5)

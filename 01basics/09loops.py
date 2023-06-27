d43fda20d7cd121bf4e902acc388e5de622d3d61
akshitagarg275 <akshitagarg275@gmail.com>
2023-06-27 21:37:16 +0530

    loops

--- 01basics/09loops.py
+++ 01basics/09loops.py
@@ -50,9 +50,62 @@ Loops -> To repeat
 
 # TODO: WAP to print reverse counting 
 
-num = 10
+# num = 10
 
-while num >= 1:
-    print(num)
-    num = num - 1 
+# while num >= 1:
+#     print(num)
+#     num = num - 1 
 
+
+# TODO: for loop 
+'''
+It is applied on the iterables
+list, tuple, dictionary, set, range
+
+iterator
+iterables -> these are the ones on which we can iterate
+iteration -> looping 
+'''
+
+fruits = ["mango", "orange", "banana", "guava"]
+
+# for fruit in fruits:
+#     print(fruit)
+
+# for i in range(5):
+#     print(i, end=" ")
+
+
+# TODO: else in for loop
+'''
+else part of the loop will be executed only if 
+loop is completely executed without any external termination
+'''
+
+# for i in range(5):
+#     print(i)
+# else:
+#     print("for loop else part")
+#     print("All elements are successfully executed")
+
+
+# TODO: WAP to print fiboannci series
+
+# 0 1 1 2 3 5 8 ......
+
+a = 0
+b = 1
+
+print(a , end =" ")
+print(b , end =" ")
+
+for i in range(2, 10):
+    c = a + b
+    a = b
+    b = c
+    print(c, end=" ")
+
+
+
+# TODO: WAP for the factorial of a given num
+# 5 = 5 *4 * 3 * 2 * 1 = 120
---
+++ 01basics/10jump_statements.py
@@ -0,0 +1,30 @@
+'''
+Jump statements -> To modify the normal flow of the program
+break -> to cut off 
+continue -> to skip the particular iteration
+pass -> bypassing
+'''
+
+# for i in range(5):
+#     if i == 3:
+#         break
+#     print(i)
+# else:
+#     print("else statements in for loop")
+#     print("It will not get executed")
+
+
+#  TODO: continue
+
+# for i in range(5):
+#     if i == 3:
+#         continue
+#     print(i)
+# else:
+#     print("Else part of the loop")
+
+
+# TODO: pass
+
+if i == 3:
+    pass
